02c3bf2e58369c9c56789a7a505f23e5400c6d62
Minh Cong <0306231007@caothang.edu.vn>
2026-07-08 12:07:01 +0700

    Update cron

--- cron/rentalStatusCron.js
+++ cron/rentalStatusCron.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { execute } from '../config/db.js';
 
-cron.schedule("*/10 * * * * *", async () => {
+cron.schedule("0 * * * * *", async () => {
     try {
         const [result] = await execute(`
             UPDATE rentalrequest
